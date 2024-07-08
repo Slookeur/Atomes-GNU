707d1e4b152f0d5830ee8d8a1b400f79b360b74d
Sébastien Le Roux <sebastien.leroux@ipcms.unistra.fr>
2024-07-08 15:07:22 +0200

    Atomes GNU: lun. 08 juil. 2024 15:07:22 CEST

--- src/workspace/version.h
+++
@@ -1,4 +0,0 @@
-#define FC "gfortran 11.4.0"
-#define FCFLAGS "-fno-second-underscore -O0 -Wall -g3 -pg -ggdb3 -cpp -dA -dD -dH -dp -dP -fvar-tracking -fbounds-check -fstack-protector-all"
-#define CC "gcc 11.4.0"
-#define CFLAGS "-O0 -Wall -g3 -pg -ggdb3 -cpp -dA -dD -dH -dp -dP -fvar-tracking -fbounds-check -fstack-protector-all -Wduplicated-cond"
